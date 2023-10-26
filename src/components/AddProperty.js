fb0343bea109eb852580725caa7798b4b860e99d
Wasiu-hub <contactbow@yahoo.com>
2023-10-26 21:18:21 +0100

    complete adding properties

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable no-console */
 import React, { useState } from "react";
 import "../styles/add-property.css";
@@ -5,7 +6,13 @@ import "../styles/add-property.css";
 const AddProperty = () => {
   const initialState = {
     fields: {
-      title: "",
+      bathroom: "1",
+      bedroom: "2",
+      city: "Manchester",
+      email: "email@yahoo.com",
+      price: "1000",
+      title: " 2 bed flat",
+      type: "Flat",
     },
   };
 
@@ -16,10 +23,57 @@ const AddProperty = () => {
     console.log(fields);
   };
 
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className="add-property">
       Add Property Page
       <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title
+          <input
+            placeholder="How many bed(s)"
+            id="title"
+            name="title"
+            value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="city">
+          City
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Leeds">Sheffield</option>
+            <option value="Leeds">Liverpool</option>
+          </select>
+        </label>
+
+        <label htmlFor="type">
+          Type
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          >
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">ShefSemi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End of Terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+        </label>
+
         <button type="submit">Add</button>
       </form>
     </div>
