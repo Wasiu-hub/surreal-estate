5bb8d5eab5167bb4127a5e11107b43ee5d0e28e3
Wasiu-hub <contactbow@yahoo.com>
2023-11-13 23:44:12 +0000

    snapshot test for three <Alert/> outcomes, but test still fails

--- src/__tests__/Alert.test.js
+++ src/__tests__/Alert.test.js
@@ -10,14 +10,17 @@ describe("Alert", () => {
     // const component = render(<Alert message="Error!" />);
     // const alertMessageNode = component.getByText("Error!");
     // expect(alertMessageNode).toBe("Error!");
-    const { getByText } = render(<Alert message="Error!" />);
+    const { getByText, asFragment } = render(<Alert message="Error!" />);
     expect(getByText(/Error/).textContent).toBe("Error!");
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test("displays a success message", () => {
-    const { getByText } = render(<Alert message="Success!!!" success />);
-
+    const { getByText, asFragment } = render(
+      <Alert message="Success!!!" success />
+    );
     expect(getByText(/Success/).textContent).toBe("Success!!!");
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test("does not render an error or a success message if message props is empty", () => {
